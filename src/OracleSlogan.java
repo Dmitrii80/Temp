9b05c917ebbf7533ea31d3420ca46f2251028af9
Dmitrii Armianovskii <a.dima80@gmail.com>
2021-06-24 22:18:54 +0500

    отредактирован текст сообщения в классе OracleSlogan

--- src/OracleSlogan.java
+++ src/OracleSlogan.java
@@ -2,6 +2,6 @@
 public class OracleSlogan {
     public static void main(String[ ] args) {
 // вывод строки
-        System.out.println("Enabling the Information Age");
+        System.out.println("Enabling the Information Age!");
     }
 }
